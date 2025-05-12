65b5471996caa13b98f074afc50308f34487b339
clxrityy <clxrityxyz@gmail.com>
2025-05-12 18:10:31 -0400

    fix imports, pass commands, change print colors

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os # Provides OS-level file and directory operations
 import shlex # Parses shell-like syntax into Python objects
 from utils.colors import color_text, Colors # Imports a custom Colors class for colored terminal output
-from utils.io import prompt, print_error, print_warning, print_success # Imports a custom prompt function for displaying the command line prompt
-from utils.fs import is_dir, list_dir, read_file # Imports custom functions for file and directory operations
+from utils.io import prompt, print_error, print_grey # Imports a custom prompt function for displaying the command line prompt
+from utils.fs import list_dir, read_file # Imports custom functions for file and directory operations
 from commands import COMMANDS, handle_command
 
 class PyKernel: # Defines a class to represent the kernel
@@ -16,14 +16,14 @@ class PyKernel: # Defines a class to represent the kernel
                 cmd = input(prompt(self.cwd))
                 self.execute(cmd)
             except (KeyboardInterrupt, EOFError): # Handles Ctrl+C and EOF (Ctrl+D) gracefully
-                print_warning("\nExiting...") # Prints a warning message when exiting
+                print_grey("\nExiting...") # Prints a warning message when exiting
                 break # Exits the loop
             except Exception as e: # Catches any other exceptions
                 print_error(e) # Prints a generic error message
                 break # Exits the loop
             
     def execute(self, cmd):
-        handle_command(cmd, shell=self) # Handles the command execution
+        handle_command(cmd, shell=self, commands=COMMANDS) # Handles the command execution
                  
             
 

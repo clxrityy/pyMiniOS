4086f90d87339bcb461d0e0aead12f4cf4df77f0
clxrityy <clxrityxyz@gmail.com>
2025-05-12 18:10:38 -0400

    add more prints

--- utils/io.py
+++ utils/io.py
@@ -29,3 +29,21 @@ def print_info(message):
     Prints an info message in blue color.
     """
     print(color_text(message, Colors.OKBLUE))  # Prints an info message in blue color
+    
+def print_heading(message):
+    """
+    Prints a heading message in bold.
+    """
+    print(color_text(message, Colors.HEADER))  # Prints a heading message in bold and blue color
+    
+def print_magenta(message):
+    """
+    Prints a message in magenta color.
+    """
+    print(color_text(message, Colors.MAGENTA))  # Prints a message in magenta color
+    
+def print_grey(message):
+    """
+    Prints a message in coral color.
+    """
+    print(color_text(message, Colors.GREY))  # Prints a message in coral color

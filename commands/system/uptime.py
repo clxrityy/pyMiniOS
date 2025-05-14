12aea4f7a26376c363e94a2903212eb9d28c71e5
clxrityy <clxrityxyz@gmail.com>
2025-05-13 22:03:48 -0400

    fix uptime

--- commands/system/uptime.py
+++ commands/system/uptime.py
@@ -1,4 +1,8 @@
 from utils.io import print_info
 
 def cmd_uptime(_args, shell):
-    print_info(f"Uptime: {shell.process_manager.uptime()} seconds")
+    uptime = shell.system_stats.get_uptime()
+    hours = int(uptime // 3600)
+    minutes = int((uptime % 3600) // 60)
+    seconds = int(uptime % 60)
+    print_info(f"Uptime: {hours}h {minutes}m {seconds}s")

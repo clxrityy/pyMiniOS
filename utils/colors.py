45aebe2b10e85490bd92631a56b106652343dfa3
clxrityy <clxrityxyz@gmail.com>
2025-05-17 14:27:22 -0400

    update colors

--- utils/colors.py
+++ utils/colors.py
@@ -4,16 +4,16 @@ class Colors:
     """
 
     # ANSI escape sequences for colors
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
+    HEADER = "\033[92m"
+    OKBLUE = "\033[34m"
+    INFO = "\033[36m"
+    OKGREEN = "\033[32m"
+    WARNING = "\033[33m"
+    FAIL = "\033[31m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
-    MAGENTA = "\033[35m"
+    MAGENTA = "\033[95m"
     GREY = "\033[37m"
 
 def color_text(text, color):
